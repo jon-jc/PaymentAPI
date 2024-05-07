4680ab17463b7ca235c3aa1548110a1bf75df700
jon-jc <jonathancho.jc@gmail.com>
2024-05-07 14:34:05 -0700

    Successful integration of backend API with PostgreSQL

---
+++ Migrations/20240507210437_UpdateIdentityForPaymentDetailId.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PaymentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateIdentityForPaymentDetailId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "varchar(3)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                type: "varchar(5)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentDetails",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                type: "varchar(16)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentDetailId",
+                table: "PaymentDetails",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "nvarchar(3)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(3)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PaymentDetails",
+                type: "nvarchar(5)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(5)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentDetails",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                type: "nvarchar(16)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentDetailId",
+                table: "PaymentDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
